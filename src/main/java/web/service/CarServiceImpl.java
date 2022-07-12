5b8c7c5b9d6c05b65b40f0b406e4411ac226973e
@MariaWhitch <dontworrybehappymary@gmail.com>
2022-07-12 10:48:07 +0400

    ImVeryHappy

---
+++ src/main/java/web/service/CarServiceImpl.java
@@ -0,0 +1,29 @@
+package web.service;
+
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CarServiceImpl implements CarService {
+
+    @Override
+    public List<Car> carList(Integer count) {
+        List<Car> carList;
+        {
+            carList = new ArrayList<>();
+            carList.add(new Car("Mazda", "6", "black"));
+            carList.add(new Car("Volvo", "S", "gray"));
+            carList.add(new Car("Marcedes", "R", "blue"));
+            carList.add(new Car("BMW", "320i", "dark"));
+            carList.add(new Car("Opel", "Astra", "white"));
+        }
+        return carList.stream().limit(count).collect(Collectors.toList());
+        }
+    }
+
+
+
+
+
